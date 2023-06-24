c5a501d8f0df3bc9ec9fb11e52dc55afd05ecba5
min8grace <56316476+min8grace@users.noreply.github.com>
2023-06-23 22:44:32 -0700

    Django Rest framework

---
+++ categories/views.py
@@ -0,0 +1,27 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
+
+@api_view(["GET", "POST"])
+def categories(request):
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        Category.objects.create(
+            name=request.data["name"],
+            kind=request.data["kind"],
+        )
+        return Response({"created": True})
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
+
+
+# Create your views here.
